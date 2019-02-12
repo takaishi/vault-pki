442382d7b72661d17404003462d208a865469812
r_takaishi <ryo.takaishi.0@gmail.com>
2019-02-12 12:21:35 +0900

    add role create command

--- cmd/role.go
+++ cmd/role.go
@@ -32,6 +32,41 @@ func RoleSubcommands() []cli.Command {
 				return nil
 			},
 		},
+		{
+			Name: "create",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "pki",
+				},
+				cli.StringFlag{
+					Name: "name",
+				},
+				cli.StringFlag{
+					Name: "organization",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				client, err := vault.NewClient()
+				if err != nil {
+					return err
+				}
+
+				data := map[string]interface{}{
+					"key_bites":         "4096",
+					"max_ttl":           "8760h",
+					"allow_any_name":    true,
+					"enforce_hostnames": false,
+					"organization":      c.String("organization"),
+				}
+
+				_, err = client.Logical().Write(fmt.Sprintf("/%s/roles/%s", c.String("pki"), c.String("name")), data)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+		},
 	}
 
 	return commands
