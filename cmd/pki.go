c5062b6c8813fda1e1913d91b27ae5f177786b86
r_takaishi <ryo.takaishi.0@gmail.com>
2019-02-12 11:36:30 +0900

    add pki disable command

--- cmd/pki.go
+++ cmd/pki.go
@@ -58,6 +58,22 @@ func PkiSubcommands() []cli.Command {
 				return client.Sys().Mount(c.String("path"), mountInput)
 			},
 		},
+		{
+			Name: "disable",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "path",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				client, err := vault.NewClient()
+				if err != nil {
+					return err
+				}
+
+				return client.Sys().Unmount(c.String("path"))
+			},
+		},
 	}
 
 	return commands
