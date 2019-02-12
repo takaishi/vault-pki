9f3a442b2fafa8e7402dbf95adf654b8524a62c8
r_takaishi <ryo.takaishi.0@gmail.com>
2019-02-12 12:34:06 +0900

    add certificate create command

--- cmd/certificate.go
+++ cmd/certificate.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"bytes"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"github.com/hashicorp/vault/helper/certutil"
 	"github.com/olekukonko/tablewriter"
 	"github.com/pkg/errors"
 	"github.com/takaishi/vault-pki/vault"
 	"github.com/urfave/cli"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -62,6 +65,82 @@ func CertificateSubcommands() []cli.Command {
 					table.Append(v)
 				}
 				table.Render()
+				return nil
+			},
+		},
+		{
+			Name: "create",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "pki",
+				},
+				cli.StringFlag{
+					Name: "role",
+				},
+				cli.StringFlag{
+					Name: "common_name",
+				},
+				cli.StringFlag{
+					Name: "organization",
+				},
+				cli.StringSliceFlag{
+					Name: "alt_names",
+				},
+				cli.StringSliceFlag{
+					Name: "ip_sans",
+				},
+				cli.StringFlag{
+					Name: "ttl",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				client, err := vault.NewClient()
+				if err != nil {
+					return err
+				}
+
+				data := map[string]interface{}{}
+
+				if c.String("common_name") != "" {
+					data["common_name"] = c.String("common_name")
+				}
+				if c.String("organization") != "" {
+					data["organization"] = c.String("organization")
+				}
+				if len(c.StringSlice("alt_names")) != 0 {
+					data["alt_names"] = strings.Join(c.StringSlice("alt_names"), ",")
+				}
+				if len(c.StringSlice("ip_sans")) != 0 {
+					data["ip_sans"] = strings.Join(c.StringSlice("ip_sans"), ",")
+				}
+				if c.String("ttl") != "" {
+					data["ttl"] = c.String("ttl")
+				}
+
+				rawCertData, err := client.Logical().Write(fmt.Sprintf("%s/issue/%s", c.String("pki"), c.String("role")), data)
+				if err != nil {
+					return err
+				}
+				certData, err := certutil.ParsePKIMap(rawCertData.Data)
+				if err != nil {
+					return err
+				}
+
+				out := &bytes.Buffer{}
+				pem.Encode(out, &pem.Block{Type: "CERTIFICATE", Bytes: certData.Certificate.Raw})
+				err = ioutil.WriteFile(fmt.Sprintf("./%d.pem", certData.Certificate.SerialNumber), out.Bytes(), 0644)
+				if err != nil {
+					return err
+				}
+
+				out2 := &bytes.Buffer{}
+				b := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: certData.PrivateKeyBytes}
+				pem.Encode(out2, b)
+				err = ioutil.WriteFile(fmt.Sprintf("./%d-key.pem", certData.Certificate.SerialNumber), out2.Bytes(), 0644)
+				if err != nil {
+					return err
+				}
+
 				return nil
 			},
 		},
