3a69bc78bc7f2f2dce78347f8aa3f6b3aad50f25
r_takaishi <ryo.takaishi.0@gmail.com>
2019-02-12 11:31:29 +0900

    add pki enable command

--- cmd/pki.go
+++ cmd/pki.go
@@ -53,6 +53,52 @@ func PkiSubcommands() []cli.Command {
 				return nil
 			},
 		},
+		{
+			Name: "enable",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "path",
+				},
+				cli.StringFlag{
+					Name: "description",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				vaultAddr := os.Getenv("VAULT_ADDR")
+				vaultToken := ""
+				vaultTokenFromEnv := os.Getenv("VAULT_TOKEN")
+
+				if vaultTokenFromEnv != "" {
+					vaultToken = vaultTokenFromEnv
+				} else {
+					vaultTokenFromCache, err := ioutil.ReadFile(fmt.Sprintf("%s/.vault-token", os.Getenv("HOME")))
+					if err != nil {
+						return err
+					}
+					vaultToken = string(vaultTokenFromCache)
+				}
+
+				var httpClient = &http.Client{
+					Timeout: 10 * time.Second,
+				}
+
+				client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
+				if err != nil {
+					return err
+				}
+				client.SetToken(vaultToken)
+
+				mountInput := &api.MountInput{
+					Type:        "pki",
+					Description: c.String("description"),
+					Config: api.MountConfigInput{
+						DefaultLeaseTTL: "87600h",
+						MaxLeaseTTL:     "87600h",
+					},
+				}
+				return client.Sys().Mount(c.String("path"), mountInput)
+			},
+		},
 	}
 
 	return commands
